d040bdba73f81bca0fa390ab42740828cb8e75a1
cxshun <cxshun@gmail.com>
2019-08-14 14:50:38 +0800

    修改vue文件

--- ebook-admin-web/src/components/user/User.vue
+++ ebook-admin-web/src/components/user/User.vue
@@ -4,31 +4,35 @@
             <el-button type="primary" size="large" @click="dialogVisible = true">添加用户</el-button>
         </el-col>
         <el-col>
-            <el-table :data="userList" stripe style="width: 100%" empty-text="暂无数据">
+            <el-table :data="userList" stripe border style="width: 100%" empty-text="暂无数据">
                 <el-table-column prop="id" label="ID" width="100"></el-table-column>
                 <el-table-column prop="name" label="姓名"></el-table-column>
-                <el-table-column prop="active" label="是否有效" width="100"></el-table-column>
+                <el-table-column prop="email" label="邮箱"></el-table-column>
+                <el-table-column prop="isActive" label="是否有效" width="100"></el-table-column>
                 <el-table-column label="操作" width="200">
-                    <template>
-                        <el-button v-if="active === 1" @click="activate(id, 1)" type="text" size="small">启用</el-button>
-                        <el-button v-else-if="active === 0" @click="activate(id, 0)" type="text" size="small">禁用</el-button>
-                        <el-button @click="del(id)" type="danger" size="small">删除</el-button>
+                    <template slot-scope="scope">
+                        <el-button v-if="scope.row.isActive === 0" @click="activate(scope.row.id, 1)" type="primary" size="small">启用</el-button>
+                        <el-button v-else-if="scope.row.isActive === 1" @click="activate(scope.row.id, 0)" type="primary" size="small">禁用</el-button>
+                        <el-button @click="del(scope.row.id)" type="danger" size="small">删除</el-button>
                     </template>
                 </el-table-column>
             </el-table>
         </el-col>
 
         <el-dialog title="新增用户" :visible.sync="dialogVisible">
-            <el-form :form="form" label-width="100px">
+            <el-form ref="user" :model="user" label-width="100px">
                 <el-form-item label="用户名称">
-                    <el-input v-model="form.name" auto-complete="off"></el-input>
+                    <el-input v-model="user.name" auto-complete="off" placeholder="请输入用户名"></el-input>
+                </el-form-item>
+                <el-form-item label="用户密码">
+                    <el-input v-model="user.password" auto-complete="off" placeholder="请输入密码"></el-input>
                 </el-form-item>
                 <el-form-item label="用户邮箱">
-                    <el-input v-model="form.email" auto-complete="off"></el-input>
+                    <el-input v-model="user.email" auto-complete="off" placeholder="请输入邮箱"></el-input>
                 </el-form-item>
             </el-form>
             <div slot="footer" class="dialog-footer">
-                <el-button @click="save(form)">保存</el-button>
+                <el-button @click="save">保存</el-button>
                 <el-button @click="dialogVisible = false">取消</el-button>
             </div>
         </el-dialog>
@@ -40,15 +44,22 @@
 
     export default {
         data() {
-            let userList = [];
-            this.$http.get(Common.url + '/admin/users').then((response) => {
-                userList = response.data.data;
-            }).catch(response => Common.postCallback(response));
             return {
-                userList: userList,
-                form: {},
-                dialogVisible: false
+                userList: [{}],
+                user: {},
+                dialogVisible: false,
+                rules: {
+                    name: [{required: true, message: '请输入用户名', trigger: blur}],
+                    password: [{required: true, message: '请输入密码', trigger: blur}],
+                    email: [{required: true, message: '请输入email', trigger: blur}]
+                }
             }
+        },
+        mounted() {
+            this.$http.get(Common.url + '/admin/users').then((response) => {
+                this.userList = response.data.data.list;
+                console.log(this.userList);
+            }).catch(response => Common.postCallback(response));
         },
         methods: {
             activate(id, operationType) {
@@ -60,9 +71,13 @@
             del(id) {
                 this.$http.delete(Common.url + "/admin/users/" + id).then((response => Common.postCallback(response)));
             },
-            save(form) {
+            save() {
+                let user = this.user;
+                console.log(user);
                 this.$http.post(Common.url + "/admin/users", {
-                    form
+                    name: user.name,
+                    password: user.password,
+                    email: user.email
                 }).then((response => Common.postCallback(response)))
             }
         }
